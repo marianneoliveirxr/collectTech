3bb3371c114b452b9bb0ee08f1e8b6032ba3398c
leh-sptech <leticia.bastos@sptech.school>
2026-03-03 21:36:38 -0300

    Inserts e selects no banco

--- banco de dados/banco de dados.sql
+++ banco de dados/banco de dados.sql
@@ -22,13 +22,13 @@ cargo VARCHAR(30) PRIMARY KEY NOT NULL
 
 -- é necessario tambem colocar a coluna na tabela em que voce quer puxar ela igual na coluna CARGO abaixo:
 CREATE TABLE IF NOT EXISTS usuario(
-id_usuario INT PRIMARY KEY NOT NULL,
+id_usuario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome VARCHAR(255) NOT NULL,
 email VARCHAR(255) NOT NULL,
 senha VARCHAR(255) NOT NULL,
-tel VARCHAR(11) NOT NULL,
-cargo VARCHAR(30) NOT NULL,
-FOREIGN KEY (cargo) REFERENCES cargos(cargo)
+tel CHAR(11) NOT NULL
+/*cargo VARCHAR(30) NOT NULL,
+FOREIGN KEY (cargo) REFERENCES cargos(cargo)*/
 );
 -- para utilizar a FOREIGN KEY, primeiro declaramos que ela é uma chave estrangeira e colocamos o nome
 -- da coluna que queremos chamar com parenteses (igual ali em cima), depois do nome da coluna a gente 
@@ -43,8 +43,8 @@ numero VARCHAR(4) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS caminhoes(
-quantidade INT NOT NULL,
-disponivel TINYINT PRIMARY KEY NOT NULL,
+id_caminhao INT PRIMARY KEY NOT NULL,
+disponivel TINYINT NOT NULL,
 placa VARCHAR(7)
 );
 
@@ -53,12 +53,42 @@ nome_lix VARCHAR(20) PRIMARY KEY NOT NULL,
 lotacao TINYINT
 );
 
-CREATE TABLE IF NOT EXISTS coleta(
+/* CREATE TABLE IF NOT EXISTS coleta(
 id INT PRIMARY KEY NOT NULL,
 id_usuario INT NOT NULL,
 nome_lix VARCHAR(20) NOT NULL,
 logradouro VARCHAR(255) NOT NULL,
 FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
 FOREIGN KEY (nome_lix) REFERENCES lixeira(nome_lix),
-FOREIGN KEY (logradouro) REFERENCES localizacao(logradouro)
-);
+FOREIGN KEY (logradouro) REFERENCES localizacao(logradouro)ggggggggg
+); */
+
+INSERT INTO cargos (cargo) VALUES ('Motorista'), 
+('Analista de dados'), ('Gestor'), ('Administrador');
+
+SELECT * FROM cargos;
+
+INSERT INTO usuario (id_usuario, nome, email, senha, tel) 
+VALUES (1,'Leticia', 'leticia.bastos@sptechschool.com', '12345', '11991476333'), 
+(2,'Andre', 'andrezinho@email.com', '1234565', '11991476334'), (3,'Samuel', 'santos@email.com', '123123', '11991472222');
+
+SELECT * FROM usuario;
+
+INSERT INTO localizacao (logradouro, cep, numero) 
+VALUES ('Rua Inocencio de Aquino', '64910000', '818'), 
+('Rua Ana Oliveira Lopes', '64910000', '389'), 
+('Rua Martins Lopes', '64910000', '438');
+
+SELECT * FROM localizacao;
+
+INSERT INTO lixeira (nome_lix, lotacao) 
+VALUES ('Inocencio 1',0), ('Ana Oliveira 1', 1), ('Martins Lopes 1',0);
+
+SELECT * FROM lixeira;
+
+INSERT INTO caminhoes (id_caminhao, disponivel, placa)
+VALUES (1, 1, 'ALC1D83'), (2, 0, 'RTS5F02'), (3, 1, 'BVE2A77'),
+(4, 0, 'JXM0B39'), (5, 1, 'KMP9E84');
+
+SELECT * FROM caminhoes;
+
